88662427986c89f4a09039106acb7c55cf9900ce
Abdul Rafay Amir <abdulrafayamir002@gmail.com>
2023-08-17 14:42:53 +0500

    Updated index.html and styles.css. Added script.js for menu toggling and removed icons folder as it was no longer needed

---
+++ Navbar&Footer/script.js
@@ -0,0 +1,11 @@
+//JS code for the toggle menu
+
+let menu= document.querySelector('#menu-icon');
+
+let navbar= document.querySelector('.navbar');
+
+menu.onclick=()=>{
+
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
